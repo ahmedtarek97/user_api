7b10a11699e4c766157157aebc71aa45e7862d80
ahmed tarek <ahmedtrk97@gmail.com>
2019-11-08 14:28:24 +0200

    handling CORS errors

--- app.js
+++ app.js
@@ -8,7 +8,14 @@ app.use(morgan('dev'));
 //extract data from the body of the requst and makes it easily readable
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+// handling CORS errors
+app.use((req,res,next)=>{
 
+res.header('Acess-Control-Allow-Origin');
+
+
+
+});
 
 app.use('/user',userRoutes);
 //requsts that was not handled by the userRoutes
