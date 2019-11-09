a5b521c9a6e7402bb7f19df3347f3d3659e1e423
ahmed tarek <ahmedtrk97@gmail.com>
2019-11-09 04:54:52 +0200

    installing jwt library

--- api/models/user.js
+++ api/models/user.js
@@ -5,9 +5,10 @@ const userSchema = mongoose.Schema({
 
 
     _id:mongoose.Schema.Types.ObjectId,
-    username:{type:String,unique : true, required : true, match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-},
-    email:{type:String, required : true},
+    username:{type:String,unique : true, required : true},
+    email:{type:String, required : true,
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
     password:{type:String, required : true},
     firstName:{type:String, required : true},
     lastName:{type:String, required : true},
